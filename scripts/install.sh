86120c482fcb44d346dfae1872bf06619250799c
Bryan Crowe <bryan@bryan-crowe.com>
2015-12-17 16:38:17 -0500

    Fill out install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+! [ -f $HOME/.zshrc.bak ] && \
+	cp $HOME/.zshrc $HOME/.zshrc.bak && \
+	echo 'Backed up zshrc to zshrc.bak'
+
+rm $HOME/.zshrc
+ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
+
+for file in $HOME/.dotfiles/*
+do
+	basefile=`basename $file`
+	if [ -f "$HOME/.$basefile" ];
+	then
+		! [ -f "$HOME/.$basefile.bak" ] && \
+			cp "$HOME/.$basefile" "$HOME/.$basefile.bak" && \
+			echo "Backed up existing .$basefile to .$basefile.bak"
+	fi
+	if ! [ "$file" == '.' ] && ! [ "$file" == '..' ]; then
+		rm "$HOME/.$basefile"
+		ln -s $file "$HOME/.$basefile"
+		echo " - Linked $basefile"
+	fi
+done
